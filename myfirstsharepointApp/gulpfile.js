71e96afc4c111ef6f546a51c0778305f873990fe
Ravi Ekkuluri <ERavi@aureustechsystems.com>
2018-10-12 23:53:31 +0530

    Automate gulp deployment with multiple build tasks

--- myfirstsharepointApp/gulpfile.js
+++ myfirstsharepointApp/gulpfile.js
@@ -3,25 +3,37 @@
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 const spsync = require('gulp-spsync-creds').sync;
+const minimist = require('minimist');
 
-build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+const environmentInfo = {
+    "username": "<production-username>",
+    "password": "<production-password>",
+    "cdnSite": "<cdn-site-relative-path>",
+};
+
+// build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+
+const options = minimist(process.argv.slice(2));
 
 
 build.task('upload-app-pkg', {
     execute: (config) => {
         return new Promise((resolve, reject) => {
             const pkgFile = require('./config/package-solution.json');
-            const folderLocation = `./sharepoint/${pkgFile.paths.zippedPackage}`;
+            // Retrieve the filename from the package solution config file
+            if (pkgFile) {
+                const folderLocation = `./sharepoint/${pkgFile.paths.zippedPackage}`;
 
-            return gulp.src(folderLocation)
-                .pipe(spsync({
-                    "username": "xxxxxxxxx",
-                    "password": "xxxxxxxxx",
-                    "site": "https://spaureus.sharepoint.com/sites/eRavi",
-                    "libraryPath": "AppCatalog",
-                    "publish": true
-                }))
-                .on('finish', resolve);
+                return gulp.src(folderLocation)
+                    .pipe(spsync({
+                        "username": environmentInfo.username,
+                        "password": environmentInfo.password,
+                        "site": environmentInfo.cdnSite,
+                        "libraryPath": "AppCatalog",
+                        "publish": true
+                    }))
+                    .on('finish', resolve);
+            }
         });
     }
 });
@@ -31,24 +43,30 @@ build.task('upload-assets', {
     execute: (config) => {
         return new Promise((resolve, reject) => {
             const deployFolder = require('./config/copy-assets.json');
-            const folderLocation = `./${deployFolder.deployCdnPath}/**/*.*`;
+            if (deployFolder) {
+                // Retrieve the deploy folder path from the assets config file
+                const folderLocation = `./${deployFolder.deployCdnPath}/**/*.*`;
 
-            return gulp.src(folderLocation)
-                .pipe(spsync({
-                    "username": "xxxxxxxxx",
-                    "password": "xxxxxxxxx",
-                    "site": "https://spaureus.sharepoint.com/sites/eRavi",
-                    "libraryPath": "CDNAssets",
-                    "publish": true
-                }))
-                .on('finish', resolve);
+                return gulp.src(folderLocation)
+                    .pipe(spsync({
+                        "username": environmentInfo.username,
+                        "password": environmentInfo.password,
+                        "site": environmentInfo.cdnSite,
+                        "libraryPath": "CDNAssets",
+                        "publish": true
+                    }))
+                    .on('finish', resolve);
+            }
         });
     }
 });
 
-
-gulp.task("deploy-sharepoint", () => runSequential(
-    ["upload-app-pkg", "upload-assets"])
+gulp.task("deploy-sharepoint", () => {
+    environmentInfo.username = options['username'];
+    environmentInfo.password = options['password'];
+    environmentInfo.cdnSite = options['cdnSite'];
+    runSequential(["upload-app-pkg", "upload-assets"])
+}
 );
 
 
@@ -63,3 +81,8 @@ function runSequential(tasks) {
         runSequential(tasks.slice(1));
     });
 }
+
+
+
+// gulp command to pass arguments dynamically
+// gulp deploy-sharepoint  --username ******* --password ******* --cdnSite *******
